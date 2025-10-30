f2f98c63627b66cc25f7ae244995b1df03c4114a
ilyass mar <ilyass.marghine22@gmail.com>
2025-10-30 16:56:59 +0100

    Feat: added the entity of RawMaterial, nnot finished yet, still figuring the relation between it and the supply order

--- src/main/java/com/supplychainx/model/RawMaterial.java
+++ src/main/java/com/supplychainx/model/RawMaterial.java
@@ -1,4 +1,45 @@
 package com.supplychainx.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.boot.SpringApplicationRunListener;
+
+import java.util.List;
+
+@Entity
+@Table(name = "raw_material")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RawMaterial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private int stock;
+
+    @Column(nullable = false)
+    private int stockMin;
+
+    @Column(nullable = false)
+    private String unit ;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "supplier_raw_material",
+            joinColumns = @JoinColumn(name = "raw_material_id"),
+            inverseJoinColumns = @JoinColumn(name = "supplier_id")
+    )
+    @ToString.Exclude
+    private List<Supplier> suppliers;
+
+    private List<SupplyOrderMaterial>  supplyOrderMaterials;
 }
