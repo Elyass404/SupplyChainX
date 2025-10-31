62d4f2a8ac333e918d4d63178df19604490aa5b1
ilyass mar <ilyass.marghine22@gmail.com>
2025-10-31 10:15:56 +0100

    Feat: added the interface of the service of the supplier, to impliment it in the class after

---
+++ src/main/java/com/supplychainx/service/SupplierService.java
@@ -0,0 +1,21 @@
+package com.supplychainx.service;
+
+import com.supplychainx.model.Supplier;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SupplierService {
+
+    Supplier createSupplier(Supplier  supplier);
+
+    Supplier updateSupplier(Supplier supplier);
+
+    void deleteSupplier(Long id);
+
+    Optional<Supplier> getSupplierById(Long id);
+
+    List<Supplier> getAllSuppliers();
+
+    boolean existsByEmail(String email);
+}
