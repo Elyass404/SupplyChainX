de72549666b0c16207fce6b3603cd44fda1bd7f5
ilyass mar <ilyass.marghine22@gmail.com>
2025-11-04 15:10:38 +0100

    Feature: Implement User DTOs (Request/Response) and MapStruct Mapper, enforcing UserRole consistency.

--- src/main/java/com/supplychainx/dto/request/UserRequest.java
+++ src/main/java/com/supplychainx/dto/request/UserRequest.java
@@ -1,4 +1,28 @@
 package com.supplychainx.dto.request;
 
+import com.supplychainx.model.enums.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
 public class UserRequest {
+
+    @NotBlank(message = "first name is required.")
+    String firstName;
+
+    @NotBlank(message = "last name is required.")
+    String lastName;
+
+    @NotBlank(message = "email is required ")
+    @Email(message = "You should insert a valid email")
+    String email;
+
+    @NotBlank(message = "password is required.")
+    String password;
+
+    @NotNull(message = "Role is required.")
+    UserRole role;
+
 }
--- src/main/java/com/supplychainx/dto/response/UserResponse.java
+++ src/main/java/com/supplychainx/dto/response/UserResponse.java
@@ -1,4 +1,20 @@
 package com.supplychainx.dto.response;
 
+import com.supplychainx.model.enums.UserRole;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
 public class UserResponse {
+
+    Long id;
+
+    String firstName;
+
+    String lastName;
+
+    String email;
+
+    UserRole role;
 }
