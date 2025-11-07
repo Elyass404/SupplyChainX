f6ce3a0daeff887529f2de14bde4f594634f843d
ilyass mar <ilyass.marghine22@gmail.com>
2025-11-07 22:34:03 +0100

    Feat(delivery): Define service interfaces for Customer, Order, and Delivery.

--- src/main/java/com/supplychainx/delivery_service/service/CustomerService.java
+++ src/main/java/com/supplychainx/delivery_service/service/CustomerService.java
@@ -1,4 +1,20 @@
 package com.supplychainx.delivery_service.service;
 
+import com.supplychainx.delivery_service.dto.request.CustomerRequest;
+import com.supplychainx.delivery_service.dto.response.CustomerResponse;
+import com.supplychainx.delivery_service.model.Customer;
+
+import java.util.List;
+
 public interface CustomerService {
+
+    CustomerResponse createCustomer(CustomerRequest request);
+
+    CustomerResponse updateCustomer(Long id, CustomerRequest request);
+
+    void  deleteCustomer(Long id);
+
+    List<CustomerResponse> getAllCustomers();
+
+    List<CustomerResponse> searchCustomersByName(String name);
 }
--- src/main/java/com/supplychainx/delivery_service/service/OrderService.java
+++ src/main/java/com/supplychainx/delivery_service/service/OrderService.java
@@ -1,4 +1,28 @@
 package com.supplychainx.delivery_service.service;
 
+import com.supplychainx.delivery_service.dto.request.OrderRequest;
+import com.supplychainx.delivery_service.dto.response.OrderResponse;
+import com.supplychainx.supply_service.model.enums.OrderStatus;
+
+import java.util.List;
+
 public interface OrderService {
+
+    OrderResponse createOrder(OrderRequest request);
+
+    OrderResponse updateOrder(Long id, OrderRequest request);
+
+    void deleteOrder(Long id);
+
+    List<OrderResponse> getAllOrders();
+
+    OrderResponse  getOrderById(Long id);
+
+    List<OrderResponse> getOrdersByStatus(OrderStatus status);
+
+    OrderResponse shipOrder(Long id);
+
+    OrderResponse completeOrder(Long id);
+
+    OrderResponse cancelOrder(Long id);
 }
