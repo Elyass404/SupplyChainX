30fa305290f45857410d0cd93acb8e62aff1334a
ilyass mar <ilyass.marghine22@gmail.com>
2025-11-09 01:46:31 +0100

    Feat: added the interface of the delivery service

--- src/main/java/com/supplychainx/delivery_service/service/DeliveryService.java
+++ src/main/java/com/supplychainx/delivery_service/service/DeliveryService.java
@@ -1,4 +1,18 @@
 package com.supplychainx.delivery_service.service;
 
+import com.supplychainx.delivery_service.dto.request.DeliveryRequest;
+import com.supplychainx.delivery_service.dto.response.DeliveryResponse;
+import com.supplychainx.supply_service.model.enums.DeliveryStatus;
+
+import java.util.List;
+
 public interface DeliveryService {
+
+    DeliveryResponse createDelivery(DeliveryRequest request);
+
+    DeliveryResponse getDeliveryById(Long id);
+
+    List<DeliveryResponse> getAllDeliveries();
+
+    DeliveryResponse updateDeliveryStatus(Long id, DeliveryStatus newStatus);
 }
