2169c1daa5678839f6153cf99ab3ae231b0e0fb1
ilyass mar <ilyass.marghine22@gmail.com>
2025-11-19 02:13:54 +0100

    Feat: added the builder annotationto make the supplier dto request able to be created using just the builder

--- src/main/java/com/supplychainx/supply_service/dto/request/SupplierRequest.java
+++ src/main/java/com/supplychainx/supply_service/dto/request/SupplierRequest.java
@@ -3,10 +3,12 @@ package com.supplychainx.supply_service.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Value;
 
 // Using @Value for immutability and automatic getter generation
 @Value
+@Builder
 public class SupplierRequest {
 
     @NotBlank(message = "Supplier name is required.")
