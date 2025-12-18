fb94589686daa9697d3ef80023f4cbca350e3971
ilyass mar <ilyass.marghine22@gmail.com>
2025-12-18 23:41:04 +0100

    Feat: created the repository for the refresh token

---
+++ src/main/java/com/supplychainx/supply_service/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package com.supplychainx.supply_service.repository;
+
+import com.supplychainx.supply_service.model.RefreshToken;
+import com.supplychainx.supply_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long > {
+
+    Optional<RefreshToken> findByToken (String token );
+
+    void deleteByUser(User user);
+}
