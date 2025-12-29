2c855872f44cc279296440882545ea7a13a4582b
ilyass mar <ilyass.marghine22@gmail.com>
2025-12-29 01:39:42 +0100

    Feat: Created the refresh token dto request to specify what should go in the request when using the endpoint of the refresh token

---
+++ src/main/java/com/supplychainx/security/auth/dto/request/RefreshTokenRequest.java
@@ -0,0 +1,15 @@
+package com.supplychainx.security.auth.dto.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshTokenRequest {
+    private String refreshToken;
+}
